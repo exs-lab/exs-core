b8d10c7a4a6bc892d378adbc8cb85c0d0bc0cae4
kipoha <busterx25@gmail.com>
2026-05-05 00:14:20 +0600

    fix depends

--- libexs/__init__.py
+++ libexs/__init__.py
@@ -5,6 +5,16 @@ Core Library for exs-shell and plugin
 __version__ = "0.0.1"
 __author__ = "kipoha"  # only github usernames
 
+
+try:
+    import ignis  # noqa: F401
+except ImportError:
+    raise ImportError(
+        "ignis is required but not installed.\n"
+        "Install it with: pip install git+https://github.com/ignis-sh/ignis.git"
+    )
+
+
 from libexs import utils, register
 
 __all__ = [
